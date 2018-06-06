fe89d731c65b276304a9ff0d39f7d114979c8358
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-06 13:29:39 -0500

    Initial commit

---
+++ source.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int * a;
+int * b;
+int * c;
+int stackA;
+int stackB;
+int stackC;
+
+void hanoi();
+
+int main() 
+{
+    int size = 5;
+
+    a = (int *) calloc(size, sizeof(int));
+    b = (int *) calloc(size, sizeof(int));
+    c = (int *) calloc(size, sizeof(int));
+    
+    stackA = 4;
+    stackB = 0;
+    stackC = 0;
+
+    for(int i = 0; i < size; i++){
+        a[i] = size - i;
+    }
+
+    hanoi();
+
+    return 0; 
+} 
