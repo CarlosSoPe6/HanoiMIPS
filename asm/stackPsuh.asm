74a80f8b0cba7b5b6d4ae28f0c433a6abba8e2b3
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-07 18:39:44 -0500

    Sack push ready

---
+++ asm/stackPsuh.asm
@@ -0,0 +1,14 @@
+# ------ [ stackPush ] ----------------------
+# 
+# Retrive and remove a element from a stack
+# param $a0:    Stack head
+# param $a1:    Stack pointer
+# param $a2:    Data to push
+stackPush:
+    lw		$t0, 0($a1)		    # Load $t0 = $a1
+    sll     $t1, $t0, 2         # $t1 = $a1 << 2
+    add		$t1, $a0, $t1		# $t1 = $a0 + $t1
+    sw		$a2, 0($t1)		    # Store *$t1 = $a2
+    addi	$a1, $a1, 1			# $a1 = $a1 + 1
+    sw		$t0, 0($a1)		    # Store *$a1 = $t0
+    jr $ra
