6e833f8f017daef48c51b3eb4b146df7eba767af
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-07 12:34:53 -0500

    hanoi tower c

--- source.c
+++ source.c
@@ -7,26 +7,94 @@ int * c;
 int stackA;
 int stackB;
 int stackC;
+int size;
 
-void hanoi();
+int stackPush(int * tower, int data);
+int stackPop(int * tower);
+void printTower(int * tower);
+void hanoi(int n, int * source, int * target, int * aux);
 
-int main() 
-{
-    int size = 5;
+int main() {
+    size = 20;
 
+    // Crean los arreglos
     a = (int *) calloc(size, sizeof(int));
     b = (int *) calloc(size, sizeof(int));
     c = (int *) calloc(size, sizeof(int));
     
-    stackA = 4;
+    // Crean los apuntadores del stack
+    stackA = size;
     stackB = 0;
     stackC = 0;
 
+    // Init tower A
     for(int i = 0; i < size; i++){
         a[i] = size - i;
     }
 
-    hanoi();
+    printTower(a);
+    printTower(b);
+    printTower(c);
+
+    hanoi(size, a, c, b);
+
+    printTower(a);
+    printTower(b);
+    printTower(c);
 
     return 0; 
 } 
+
+/**
+ * Recusrive call 
+ * 
+ */
+void hanoi(int n, int * source, int * target, int * aux){
+    if(n > 0){
+        hanoi(n - 1, source, aux, target);
+        int d = stackPop(source);
+        stackPush(target, d);
+        hanoi(n - 1, aux, target, source);
+    }
+}
+
+void printTower(int * tower){
+    for(int j = 0; j < size; j++){
+        printf("%d ", tower[j]);            
+    }
+    printf("\n");
+}
+
+int stackPush(int * tower, int data){
+    if(tower == a){
+        tower[stackA] = data;
+        stackA++;
+    }else if(tower == b){
+        tower[stackB] = data;
+        stackB++;
+    }else{
+        tower[stackC] = data;
+        stackC++;
+    }
+}
+
+int stackPop(int * tower){
+    int ret;
+    if(tower == a){
+        if(stackA != 0)
+            stackA--;
+        ret = tower[stackA];
+        tower[stackA] = 0;
+    }else if(tower == b){
+        if(stackB != 0)
+            stackB--;
+        ret = tower[stackB];
+        tower[stackB] = 0;
+    }else{
+        if(stackC != 0)
+            stackC--;
+        ret = tower[stackC];
+        tower[stackC] = 0;
+    }
+    return ret;
+}
