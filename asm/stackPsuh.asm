3ca9842ed79e2c15b7f328448b014774a908fa3e
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-09 22:49:49 -0500

    issue with refs

--- asm/stackPsuh.asm
+++ asm/stackPsuh.asm
@@ -7,5 +7,6 @@ stackPush:
     lw		    $t0, 0($s0)		# Load the stack's reference
     sw		    $a1, 0($t0)		# Store at stack's reference
     addi	    $t0, $t0, -4	# $t0 = $t0 + -4
+    sw          $t0, 0($s0)     # Store the new stack reference
     jr		    $ra				# jump to $ra  
        
