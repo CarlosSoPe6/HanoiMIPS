25482bbc924d579318b1cda21bd672ccadd4ae6d
Carlos Soto Pérez <carlos348@outlook.com>
2018-06-10 19:28:49 -0500

    stacks references created

--- hanoiTowers.asm
+++ hanoiTowers.asm
@@ -1,48 +1,65 @@
 # hanoiTowers.asm
 # Author: Carlos Soto PÃ©rez
-# Author: Salvador Cortez González
+# Author: Salvador Cortez Gonzï¿½lez
 .text    
 #------------------------------[Main function]-----------------------------------
 #
 # Initialize stack pointers to indicate where the stacks are located, define the
 # number of plates and the index for every stack
-
-	ori 	$a0, $at , 0x1001 	
-	sll     $a0, $a0, 16
-	addi 	$a0, $a0 , 0x0000 	# Pointer to stack A
-	ori 	$a1, $at , 0x1001 	
-	sll     $a1, $a1, 16
-	addi 	$a1, $a1 , 0x0050       # Pointer to stack B
-	ori 	$a2, $at , 0x1001 	
-	sll     $a2, $a1, 16
-	addi 	$a2, $a2 , 0x00A0       # Pointer to stack c  
-		
 main:	
+	addi 	$at, $zero , 0x1001 	
+	sll     $at, $at, 16
+	ori 	$t0, $at , 0x0000 		# Stack A base
+	
+	addi 	$at, $zero , 0x1001 	
+	sll     $at, $at, 16
+	ori 	$t1, $at , 0x0080       # Stack B base
+	
+	addi 	$at, $zero , 0x1001 	
+	sll     $at, $at, 16
+	ori 	$t2, $at , 0x0100       # Stack C base
 
-        addi 	$a3, $zero, 3		# N= Number of plates
-	add 	$s0, $zero, $a3         # Stack A index
-	addi 	$s1, $zero, 0	        # Stack B index
-	addi 	$s2, $zero, 0	        # Stack C index
-	addi	$s3, $zero, -1          # k (for counter)
+	# Store the int **
+	addi 	$at, $zero , 0x1001 	
+	sll     $at, $at, 16
+	ori		$t3, $at, 0x0180
+	add		$a0, $zero, $t3			# $a0 = $zero + $t3
+									# the reference in a arg source
+	sw		$t0, 0($t3)				# Store the stack A reference
+	
+	addi	$t3, $t3, 4				# $t3 = $t3 + 4
+	add		$a1, $zero, $t3			# $a1 = $zero + $t3
+									# the reference in a arg aux
+	sw		$t1, 0($t3)				# Store the stack B reference
+	
+	addi	$t3, $t3, 4				# $t3 = $t3 + 4
+	add		$a2, $zero, $t3			# $a2 = $zero + $t3
+									# the reference in a arg ttarget
+	sw		$t2, 0($t3)				# Store the stack C reference
+
+	addi	$a3, $zero, 3			# $a3 = $zero + 3
+									# Number of d
+	
 
 # ----------------------------[Filling clycle]------------------------------------
 #
-# Fill stackA with the N plates and his values
+# Fill stackA with the N plates and its values
 # param s3: for's counter = k
-		
+	lw		$t0, 0($a0)			# Load A stack reference 
+	add		$t1, $zero, $a3		# $t1 = $zero + $a3
+								# $t0 is a counter	
+	
 for: 
-	sub 	$t0, $s3, $a3	        # for(k=0; k < N; k++)
-	srl     $t0, $t0, 31
-        addi    $s3, $s3, 1             # k++
-        beq     $t0, $zero, endfor      # if t0==1 --> endfor
-        sub     $t2, $a3, $s3  
-        sll	$t1, $s3, 2    	        # Shift left i
-	add 	$t1, $t1, $a0           # Adding an offset to the base address
-	sw 	$t2, 0($t1)             # Loading data from memory (vector1)
-        j 	for
+	beq		$t1, $zero, endfor			# if $t1 == $t0 then endfor
+	sw 		$t1, 0($t0)             # Loading data from memory (vector1)
+	addi	$t0, $t0, 4				# $t0 = $t0 + 4
+	addi    $t1, $t1, -1            # k++
+	j 		for
 endfor:               
-       
+    sw		$t0, 0($a0)				# Store the new reference
+	
 	jal	hanoi
+	jal end
 
 #------------------------------[Hanoi Functio]--------------------------------------
 #
@@ -53,40 +70,38 @@ endfor:
 # param a3: number of plates = N
 
 hanoi: 
-        add 	$s0, $zero, $a3
-        slti 	$t0, $a3, 1 		# Execute until N=0
-	beq 	$t0, $zero, loop 	# Branch to loop
+	bne		$a3, $zero, loop	# if $a3 != $zero then loop
 	jr      $ra
 
 loop: 
         addi    $sp, $sp, -20           # Decreasing stack pointer
-        sw	$ra, 0($sp)             # Storing return address
-        sw	$a0, 4($sp)             # Storing stackA pointer 
-        sw	$a1, 8($sp)             # Storing stackB pointer
+        sw		$ra, 0($sp)             # Storing return address
+        sw		$a0, 4($sp)             # Storing stackA pointer 
+        sw		$a1, 8($sp)             # Storing stackB pointer
         sw      $a2, 12($sp)            # Storing stackC pointer
         sw      $a3, 16($sp)            # Storing N
-        subi    $a3, $a3, 1             # Decrement N
-        jal     hanoi
+        addi 	$a3, $a3, -1
         
-        jal	stackPop                # Taking one plate
-        add     $t2, $t2, $a1           # Backing aux value
-	add	$a1, $zero, $v0		# $a1 = $zero + $v0
-					# Load from source
-        jal	stackPush               # Placing one plate
+		jal     hanoi
+        
+        jal		stackPop                # Taking one plate
+        add     $t2, $zero, $a1         # Backing aux value
+		add		$a1, $zero, $v0			# $a1 = $zero + $v0
+										# Load from source
+        jal		stackPush               # Placing one plate
         add     $a1, $zero, $t2         # Restoring aux value
                                            
-        add	$t3, $zero, $a1         # Backing aux
-        add	$t4, $zero, $a2         # Backing target
-        add	$a2, $zero, $a0         # Now a2 --> source
-        add	$a1, $zero, $t4         # Now a1 --> target
-        add	$a0, $zero, $t3         # Now a0 --> aux
-         
-        subi    $a3, $a3, 1             # Decrement N
+        add		$t3, $zero, $a1         # Backing aux
+        add		$t4, $zero, $a2         # Backing target
+        add		$a2, $zero, $a0         # Now a2 --> source
+        add		$a1, $zero, $t4         # Now a1 --> target
+        add		$a0, $zero, $t3         # Now a0 --> aux
+        addi 	$a3, $a3, -1            # Decrement N
         jal     hanoi
         
-        lw	$ra, 0($sp)         	# Loading return address
-        lw	$a0, 4($sp)             # Loading stackA pointer
-        lw	$a1, 8($sp)         	# Loading stackB pointer
+        lw		$ra, 0($sp)         	# Loading return address
+        lw		$a0, 4($sp)             # Loading stackA pointer
+        lw		$a1, 8($sp)         	# Loading stackB pointer
         lw      $a2, 12($sp)        	# Loading stackC pointer
         lw      $a3, 16($sp)        	# Loading N
         addi    $sp, $sp, 20        	# Increasing stack pointer
@@ -99,12 +114,12 @@ loop:
 # param     $a0:    Stack ref
 # return    $v0:    Element retrived  
 stackPop:
-    	lw	$t0, 0($a0)		# Load the stack's reference
-    	addi	$t0, $t0, 4		# $t0 = $t0 + 4
-    	lw	$v0, 0($v0)		# Load the stack value
-    	sw      $t0, 0($a0)     	# Store the new stack reference
-	lw	$t0, 0($zero)		# Clear stack
-    	jr	$ra			# jump to $ra 
+	lw		$t0, 0($a0)		# Load the stack's reference
+	addi	$t0, $t0, -4	# $t0 = $t0 + -4
+	lw		$v0, 0($t0)		# Load the stack value
+	sw      $t0, 0($a0)     # Store the new stack reference
+	sw		$zero, 0($t0)	# Clear stack
+	jr		$ra				# jump to $ra 
     
 
 # ------ [ stackPush ] ----------------------
@@ -113,10 +128,11 @@ stackPop:
 # param     $a2:    Stack ref
 # param     $a1:    Data to push
 stackPush:
-    	lw	 $t0, 0($a2)		# Load the stack's reference
-   	sw	 $a1, 0($t0)		# Store at stack's reference
-    	addi	 $t0, $t0, -4		# $t0 = $t0 + -4
-    	jr	 $ra		        # jump to $ra  
-       
+	lw	 	$t0, 0($a2)		# Load the stack's reference
+	sw	 	$a1, 0($t0)		# Store at stack's reference
+	addi	$t0, $t0, 4		# $t0 = $t0 + 4
+	sw		$t0, 0($a2)		# Store the stack's reference
+	jr	 	$ra		        # jump to $ra  
+	
 
-exit:
+end:
